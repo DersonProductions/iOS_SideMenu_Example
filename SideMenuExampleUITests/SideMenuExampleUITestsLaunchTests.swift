eb1212d7dfa28b5f9879f5afed105b3bb34c4b0b
Derson Productions, LLC <77082142+DersonProductions@users.noreply.github.com>
2024-08-04 22:31:09 -0400

    made some refactoring
    ok, maybe this will work finally so I can test a matrix approach

--- SideMenuExampleUITests/SideMenuExampleUITestsLaunchTests.swift
+++ SideMenuExampleUITests/SideMenuExampleUITestsLaunchTests.swift
@@ -1,6 +1,6 @@
 //
-//  SideMenu_ExUITests.swift
-//  SideMenu_ExUITests
+//  SideMenuExampleUITestsLaunchTests.swift
+//  SideMenuExampleUITests
 //
 //  Created by Derson Productions, LLC on 2024-AUG-03.
 //
@@ -32,35 +32,26 @@
 
 import XCTest
 
-final class SideMenu_ExUITests: XCTestCase {
+final class SideMenuExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
 }
